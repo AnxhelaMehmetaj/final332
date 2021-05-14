b6ea5bf4a7e6c7cc934df4fa5b46841bdcd44404
Anxhela Mehmetaj <anxhela.mehmetaj@hotmail.com>
2021-05-14 11:29:40 -0400

    authentication added

--- src/nav/NavBar.jsx
+++ src/nav/NavBar.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import {NavLink} from "react-router-dom";
 import SignedInMenu from "./SignedInMenu";
 import SignedOutMenu from "./SignedOutMenu";
 
 export default function NavBar({setFormOpen}) {
+    const [authenticated, setAuthenticated] = useState(false);
     return (
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} to='/' header>
-
                     NJIT events
                 </Menu.Item>
                 <Menu.Item as={NavLink} to='/events' name='Events' />
-                <Menu.Item>
-                    <Button onClick={() => setFormOpen(true)} positive inverted content='Create Event' />
-                </Menu.Item>
-                 <SignedOutMenu/>
-                <SignedInMenu/>
+                {authenticated && (
+                    <Menu.Item as={NavLink} to='/createEvent'>
+                        <Button positive inverted content='Create Event' />
+                    </Menu.Item>
+                )}
+                {authenticated ? (
+                    <SignedInMenu setAuthenticated={setAuthenticated} />
+                ) : (
+                    <SignedOutMenu setAuthenticated={setAuthenticated} />
+                )}
 
             </Container>
         </Menu>
